2d49ffdbc4993ce35e6545b9100f677b71d7f11a
Maylon de Oliveira <oliveira_maylon@hotmail.com>
2021-03-09 15:24:22 -0300

    fd

--- php/inscrever.php
+++ php/inscrever.php
@@ -6,7 +6,6 @@ $para = "gcarlossantana329@gmail.com";
 
 $assunto = "Contato do Curso de Marketing";
 $email = $_POST['email'];
-$nome = $email;
 $tel = $_POST['tel'];
 
  //4 – Agora definimos a  mensagem que vai ser enviado no e-mail
@@ -14,27 +13,11 @@ $tel = $_POST['tel'];
 $mensagem .= "E-mail: </strong>".$email;
 $mensagem .= "<br>  <strong>Telefone: </strong>".$tel;
 
-//5 – agora inserimos as codificações corretas e  tudo mais.
-$headers =  "Content-Type:text/html; charset=UTF-8\n";
-$headers .= "From:  ".$nome."<$email>\n";
-//Vai ser //mostrado que  o email partiu deste email e seguido do nome
-//email do servidor //que enviou
-$headers .= "X-Mailer: PHP  v".phpversion()."\n";
-$headers .= "X-IP:  ".$_SERVER['REMOTE_ADDR']."\n";
-$headers .= "Return-Path:  <$email>\n";
-//caso a msg //seja respondida vai para  este email.
-$headers .= "MIME-Version: 1.0\n";
+$header = "MIME-Version: 1.0\n";
+$header .= "Content-type: text/html; charset=utf-8\n";
+$header .= "from: $email\n";
 
-$envio = mail($para, $assunto, $mensagem, $headers);  //função que faz o envio do email.
-
-if($envio){
-    echo ("<SCRIPT LANGUAGE='JavaScript'>
-    window.alert('Mensagem enviada com sucesso')
-    window.location.href='../obrigado.html';
-    </SCRIPT>");
-}else{
-    echo "A mensagem não pode ser enviada";
-}
+$envio = mail($para, $assunto, $mensagem, $header);  //função que faz o envio do email.
 
 
 //EMAIL PARA O CLIENTE
@@ -51,40 +34,26 @@ $nomeC = "Gabriel Carlos";
 $mensagemC = "Aqui está seu acesso à Aula 01 do Curso Grátis, um Curso que vai te ensinar o passo a
 passo de como utilizar as duas melhores ferramentas para fazer vendas todos os dias,
 sem que precise investir muito dinheiro ou aparecer na internet!
-Para acessar é só clicar nesse link: "
-$mensagemC .= "<br> LINK AULA 01 ->: </strong> https://go.hotmart.com/H48483774R?ap=7144";
-$mensagemC .= "<br> LINK AULA 02 ->: </strong> https://go.hotmart.com/H48483774R?ap=225b";
-$mensagemC .= "<br> LINK AULA 03 ->: </strong> https://go.hotmart.com/H48483774R?ap=2f25";
-$mensagemC .= "<br>  <strong>E para acelerar ainda mais seus resultados e te ajudar a criar seu negócio online, eu
-preparei algumas coisas especiais pra você!
-Desde 2016, eu tenho ajudado pessoas a ganharem dinheiro com o Marketing Digital.
-Já foram mais de 7 MIL alunos que eu tive a honra de contribuir para conquistarem a 
-sua independência financeira. Por isso, eu quero dizer o que você pode esperar de mim
-agora que está na minha lista.
-Eu vou te enviar um material que vai te dar a possibilidade de dar seu primeiro passo
-para alcançar a sua liberdade. Saindo do zero, você vai aprender a utilizar as duas
-ferramentas que usei para começar do nada, sem quase nenhum conhecimento de
-marketing digital, para faturar mais de UM MILHÃO de reais sem ter que aparecer.
-E eu sei que isso é extremamente importante para você porque eu já me senti
-exatamente como você! Eu já vivi a frustração de não ter a vida que eu desejava e
-sentia a necessidade de poder me libertar. Além disso, tinha muita vergonha de
-aparecer e medo do que as pessoas pensariam de mim.
-Tanto que no próximo email que eu vou te enviar, eu vou te mostrar como criar, na
-prática, a primeira campanha de FacebookAds. Uma ferramenta que vai te permitir ter
-uma renda trabalhando como afiliado, sem ter que se expor na internet!
-";
+Para acessar é só clicar nesse link: <br> <br>";
+$mensagemC .= "<br> <strong>LINK AULA 01 ->: </strong> https://go.hotmart.com/H48483774R?ap=7144";
+$mensagemC .= "<br> <strong>LINK AULA 02 ->: </strong> https://go.hotmart.com/H48483774R?ap=225b";
+$mensagemC .= "<br> <strong>LINK AULA 03 ->: </strong> https://go.hotmart.com/H48483774R?ap=2f25";
+
 
 //5 – agora inserimos as codificações corretas e  tudo mais.
-$headersC =  "Content-Type:text/html; charset=UTF-8\n";
-$headersC .= "From:  ".$nomeC."<$emailC>\n";
-//Vai ser //mostrado que  o email partiu deste email e seguido do nome
-//email do servidor //que enviou
-$headersC .= "X-Mailer: PHP  v".phpversion()."\n";
-$headersC .= "X-IP:  ".$_SERVER['REMOTE_ADDR']."\n";
-$headersC .= "Return-Path:  <$emailC>\n";
-//caso a msg //seja respondida vai para  este email.
-$headersC .= "MIME-Version: 1.0\n";
+$headersC = "MIME-Version: 1.0\n";
+$headersC .= "Content-type: text/html; charset=utf-8\n";
+$headersC .= "from: gcarlossantana329@gmail.com\n";
+
+
 $envioCliente = mail($paraC, $assuntoC, $mensagemC, $headersC);  //função que faz o envio do email.
 
-?>
+if($envio){
+    echo ("<SCRIPT LANGUAGE='JavaScript'>
+    window.location.href='../obrigado.html';
+    </SCRIPT>");
+}else{
+    echo "A mensagem não pode ser enviada";
+}
 
+?>
